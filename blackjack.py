f712a86a14b78d2ec002d639927c67f849b0222f
UglyClancy <duncan.brandx@gmail.com>
2021-07-19 16:31:34 -0700

    Deck of cards version 1

---
+++ blackjack.py
@@ -0,0 +1,5 @@
+dealers_hand = {}
+players_hand = {}
+
+deck = {'king of hearts': 10, 'king of diamonds': 10, 'king of spades': 10, 'king of clubs': 10, 'queen of hearts': 10, 'queen of diamonds': 10, 'queen of spades': 10, 'queen of clubs': 10, 'jack of hearts': 10, 'jack of diamonds': 10, 'jack of spades': 10, 'jack of clubs': 10, 'nine of hearts': 10, 'nine of diamonds': 10, 'nine of spades': 10, 'nine of clubs': 10}
+
